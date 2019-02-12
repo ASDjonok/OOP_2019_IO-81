c5608107b057c4a121515f0e2e4c956fdc6619dd
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-02-12 09:55:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();*/
+//        read(s);
+        int a = 0;
+        int b = 2;
+//        int m = scanner.nextInt();
+        System.out.println("Hello world!");
+        if (a == 0) {
+            System.out.println("Error, sorry:(");
+        } else {
+            System.out.println(1/a);
+        }
+    }
+
+}
